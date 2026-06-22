8c7c5a1200c5005792351ada0c086f38cfb6065d
NicolasOlmos222 <nicolas0olmos@gmail.com>
2026-06-22 09:51:04 -0300

    act

--- script.js
+++ script.js
@@ -15,11 +15,21 @@ function guardarDatos(){
 
     console.log("Edad: " + edad);
 
-    document.getElementById("resultado").innerHTML = "<h2>Nombre: " + nombre + "</h2>";
+    document.getElementById("resultado").style.display = "block";
+
+    document.getElementById("resultado").innerHTML += "<h2>Nombre: " + nombre + "</h2>";
     document.getElementById("resultado").innerHTML += "<h2>Edad: " + edad + "</h2>";
     document.getElementById("resultado").innerHTML += "<button onclick='borrar()'>Borrar</button>";
 }
 
 function borrar(){
     document.getElementById("resultado").innerHTML = "";
+}
+
+function cambiarPag(pagina){
+    if(pagina == 1){
+        window.location.href = "index.html";
+    }else if(pagina == 2){
+        window.location.href = "otraPagina.html";
+    }
 }

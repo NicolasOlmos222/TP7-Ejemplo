51a8f40b8f30027aede29679d6b40c7ab0e21458
NicolasOlmos222 <nicolas0olmos@gmail.com>
2026-06-22 09:34:16 -0300

    ejemplo

---
+++ script.js
@@ -0,0 +1,25 @@
+let nombre = "";
+let edad = 0;
+
+function guardarDatos(){
+    console.log("FUNCION GUARDAR DATOS");
+
+    nombre = document.getElementById("nombre").value;
+    edad = parseInt(document.getElementById("edad").value);
+
+    if(nombre == ""){
+        console.log("El nombre esta vacio");
+    }else{
+        console.log("Nombre: " + nombre);
+    }
+
+    console.log("Edad: " + edad);
+
+    document.getElementById("resultado").innerHTML = "<h2>Nombre: " + nombre + "</h2>";
+    document.getElementById("resultado").innerHTML += "<h2>Edad: " + edad + "</h2>";
+    document.getElementById("resultado").innerHTML += "<button onclick='borrar()'>Borrar</button>";
+}
+
+function borrar(){
+    document.getElementById("resultado").innerHTML = "";
+}
